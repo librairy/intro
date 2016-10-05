3397d90691b023a67c47c03574ea7c913fea3a5c
Carlos Badenes <cbadenes@gmail.com>
2016-10-05 16:35:34 +0200

    added spring boot

---
+++ src/main/java/org/librairy/intro/Application.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016. Universidad Politecnica de Madrid
+ *
+ * @author Badenes Olmedo, Carlos <cbadenes@fi.upm.es>
+ *
+ */
+
+package org.librairy.intro;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+/**
+ * Created on 21/05/16:
+ *
+ * @author cbadenes
+ */
+@SpringBootApplication
+public class Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    static int port = 80;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public static EmbeddedServletContainerFactory getTomcatEmbeddedFactory(){
+        TomcatEmbeddedServletContainerFactory servlet = new TomcatEmbeddedServletContainerFactory();
+        servlet.setPort(port);
+        return servlet;
+    }
+
+    public static void main(String[] args){
+        try {
+
+            if (args != null && args.length > 0){
+
+                port = Integer.valueOf(args[0]);
+            }
+
+            SpringApplication.run(Application.class, args);
+
+            LOG.info("Listening on port: " + port);
+
+        } catch (Exception e) {
+            LOG.error("Error executing test",e);
+            System.exit(-1);
+        }
+
+    }
+}
